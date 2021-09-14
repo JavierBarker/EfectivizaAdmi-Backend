99ff2570c61296550a2ad1d7cf11e085a9e7fea8
José López <jlopez-2019044@kinal.edu.gt>
2021-09-14 10:44:29 -0600

    Se empezó con la consullta del préstamo

--- src/routes/loan.routes.js
+++ src/routes/loan.routes.js
@@ -12,5 +12,6 @@ api.get('/getClientLoans/:idUser',md_autentication.ensureAuth, loanController.ge
 api.get('/getUserLoans',md_autentication.ensureAuth, loanController.getUserLoans);
 api.get('/getLoans',md_autentication.ensureAuth, loanController.getLoans);
 api.put('/editLoan/:idLoan',md_autentication.ensureAuth,loanController.editLoan);
+api.get('/getLoan/:loanId',/*md_autentication.ensureAuth,*/loanController.getLoan);
 
 module.exports = api;
