8efcc304ecf9da8fc3d1a5b5cc8f272ae2119d06
Javier Barker <hernandez.javier2003@gmail.com>
2021-09-17 23:14:08 -0600

    Se creó el CRUD de Prestamo Backend

--- src/routes/loan.routes.js
+++ src/routes/loan.routes.js
@@ -13,5 +13,7 @@ api.get('/getUserLoans',md_autentication.ensureAuth, loanController.getUserLoans
 api.get('/getLoans',md_autentication.ensureAuth, loanController.getLoans);
 api.put('/editLoan/:idLoan',md_autentication.ensureAuth,loanController.editLoan);
 api.get('/getLoan/:loanId',/*md_autentication.ensureAuth,*/loanController.getLoan);
+api.get('/getLoanById/:loanId',md_autentication.ensureAuth,loanController.getLoanById);
+api.delete('/deleteLoanById/:loanId', md_autentication.ensureAuth, loanController.deleteLoanById);
 
 module.exports = api;
